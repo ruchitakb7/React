25aad9ba90c0199e32e2ac4ea1d49a694118f8c7
ruchitakb7 <ruchitakb7@gmail.com>
2024-09-25 15:01:04 +0530

    project setup

---
+++ project-one/src/App.js
@@ -0,0 +1,24 @@
+
+import ExpenseItem  from "./component/ExpenseItem";
+
+function App() {
+
+  return (
+ 
+   <div>
+ 
+    <h1>Let's get Started</h1>
+ 
+    <p>I am learning React</p>
+
+    <ExpenseItem></ExpenseItem>
+ 
+   </div>
+ 
+  );
+ 
+ }
+ 
+ 
+ 
+ export default App;
---
+++ project-one/src/component/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem()
+{
+    return(
+        <ul>
+            <li>pen-10</li>
+            <li>book-100</li>
+        </ul>
+    )
+}
+
+export default ExpenseItem
