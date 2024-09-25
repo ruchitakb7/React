f07ebed808925c31017a55d53157d295e9c16577
ruchitakb7 <ruchitakb7@gmail.com>
2024-09-25 16:51:31 +0530

    styling component

--- project-one/src/component/ExpenseItem.js
+++ project-one/src/component/ExpenseItem.js
@@ -1,13 +1,19 @@
+
+
+import "./ExpenseItem.css"
+
 function ExpenseItem()
 {
     return(
-        <div>
+        <div className="expense-item">
               <div>15 August 2023</div>
-              <div>
-               <h2>Book</h2>
-               <p>$10</p>
-               <p>Delhi</p>
+              <div className="expense-item__location">Delhi</div>
+              <div className="expense-item__description">
+               <h2 >Book</h2>
+               <p className="expense-item__price">$10</p>
+              
               </div>
+            
         
         </div>
     )
