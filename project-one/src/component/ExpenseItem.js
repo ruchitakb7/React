f611c73a93299f0a2f1d6d3f0c5f8a5f6bc92a6f
ruchitakb7 <ruchitakb7@gmail.com>
2024-09-25 17:44:20 +0530

    Props Object & Passing Data to Child Componen

--- project-one/src/component/ExpenseItem.js
+++ project-one/src/component/ExpenseItem.js
@@ -2,21 +2,19 @@
 
 import "./ExpenseItem.css"
 
-function ExpenseItem()
+function ExpenseItem(props)
 {
-    const expenseDate = new Date(2023, 7, 15).toISOString();
-  const expenseTitle = "Insurance";
-  const expenseAmount = 50;
-  const expenseLocation="Bangalore";
+   
+    
     return(
         <div className="expense-item">
-      <div>{expenseDate}</div>
-      <div className="expense-item__location">{expenseLocation}</div>
-      <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <div>{props.date.toISOString()}</div>
+        <div className="expense-item__location">{props.location}</div>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.price}</div>
+        </div>
       </div>
-    </div>
     )
 }
 
