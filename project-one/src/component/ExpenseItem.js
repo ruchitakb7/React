d2afd00a9b0913e09c6a21cd7caad76d045f5ee0
ruchitakb7 <ruchitakb7@gmail.com>
2024-09-26 21:11:50 +0530

    Formatting Date - Giving Calendar Look

--- project-one/src/component/ExpenseItem.js
+++ project-one/src/component/ExpenseItem.js
@@ -1,21 +1,31 @@
 
 
-import "./ExpenseItem.css"
+import "./ExpenseItem.css";
+
+const m=['January','February','March','April','May','June','July',
+        'August','September','October','November','December']
 
 function ExpenseItem(props)
 {
-   
-    
-    return(
-        <div className="expense-item">
-        <div>{props.date.toISOString()}</div>
-        <div className="expense-item__location">{props.location}</div>
-        <div className="expense-item__description">
+  return(
+    <div className="expense-item">
+      
+        <div>
+        <div>{m[props.date.getMonth()]}</div>
+        <div>{props.date.getFullYear()}</div>
+        <div>{props.date.getDate()}</div>
+        </div>
+      
+        <div className="expense-item__description" >
           <h2>{props.title}</h2>
-          <div className="expense-item__price">${props.price}</div>
+        <div className="expense-item__price">${props.price}</div>
+   
         </div>
       </div>
-    )
+   
+    
+  )
+  
 }
 
 export default ExpenseItem
